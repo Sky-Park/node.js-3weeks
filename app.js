e5703308f2f7053aa5ec5fb19d0ac5bf0c94ffc9
Sky-Park <alsrb7018@gmail.com>
2022-10-05 19:40:15 +0900

    필요없는 코드 삭제

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express"); //Express.js 사용할거다~~
 const port = 3000;
-const { Schema } = require("mongoose");//mongoose 에서 가져올거다~
 const app = express(); //app은 express 함수 사용할거다 라는 객체 선언
 const connect = require("./schemas/index.js");
 connect();
